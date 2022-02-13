f815b2f6fede8ebe580a593f76b18655b7793e95
Erik Plachta <eplachta@gmail.com>
2022-02-13 17:03:01 -0500

    Add handlebars helper for shrinking content on homnepage

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,27 @@
 
 const format_date = date => {
-  return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-    date
-  ).getFullYear()}`;
+  return `
+  ${new Date(date)
+    .getMonth() + 1}/${new Date(date)
+    .getDate()}/${new Date(date)
+    .getFullYear()
+  }`;
+}
+
+
+const shrinkContent = content => {
+  if (content.length > 50)
+    return content.substring(0,50) + '...';
+  return content;
 }
 
 module.exports = {
-  format_date,   
+  format_date,
+  shrinkContent
 }
   
+// Handlebars.registerHelper('dotdotdot', function(str) {
+//   if (str.length > 10)
+//     
+//   return str;
+// });
