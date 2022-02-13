057ff646396afa6f23f20ae298db4c26913dc6e8
Erik Plachta <eplachta@gmail.com>
2022-02-13 11:12:27 -0500

    Add JAWSDB_URL env variable to seeds/connection_sequelize.js

--- seeds/connection_sequelize.js
+++ seeds/connection_sequelize.js
@@ -2,18 +2,29 @@
 const Sequelize = require('sequelize'); // for drop add tables in employee_db
 require('dotenv').config(); //-- for local variable caching
 
-//-- MAKE sequelize obj to play as ORM
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.SERVER_PATH,
-    dialect: 'mysql',
-    port: 3306
-  },
-  console.log(`//-- Connection MYSQL database with npm package Sequelize success!`)
-);
+/*
+  Create connection to our database, pass in your MySQL information for username
+    and password.
+*/
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} 
+else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    },
+    console.log(`//-- Connection MYSQL database with npm package Sequelize success!`)
+  );
+}
 
 //-- exporting created sequelize obj
 module.exports = sequelize;
