55cc439be10ba8853a3da422d8bbe251adcad32f
Erik Plachta <eplachta@gmail.com>
2022-03-02 12:22:39 -0500

    verified locally, trying on  herokui

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
   //-- if user provided a username and password inform, attempt to login
   if (email && password) {
-    const response = await fetch('../../api/users/login', {
+    const response = await fetch('api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
