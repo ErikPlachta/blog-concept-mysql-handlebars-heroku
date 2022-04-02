51873bf83e9ff5d02975d419b69908d6846e2ad7
Erik Plachta <eplachta@gmail.com>
2022-04-02 14:45:35 -0400

    Add disable post  until meet requirements styling and logic

--- public/js/post.js
+++ public/js/post.js
@@ -33,7 +33,38 @@ const postNewPost = async (event) => {
         //TODO: 04/02/2022 #EP || Add refresh once posted
 }
 
-document.querySelector('.post-new-post').onsubmit = event => {
+
+const _canPost = event => {
+  // console.log(event.target.value);
+
+  const titleLength = (document.querySelector('#post-new-post-title').value).length;
+  const bodyLength = (document.querySelector('#post-new-post-text').value).length;
+
+  if(titleLength < 5){
+    document.querySelector('#post-new-post-button').disabled=true;  
+    return null;
+  };
+  if(bodyLength < 10){
+    document.querySelector('#post-new-post-button').disabled=true;  
+    return null;
+  };
+  
+  document.querySelector('#post-new-post-button').disabled=false;
+
+}
+
+window.addEventListener('load', (event) => {
+  
+  //-- Disable Post Button by default
+  document.querySelector('#post-new-post-button').disabled=true;  
+
+  //-- See if should still be disabled
+  document.querySelector('#post-new-post-text').addEventListener("keyup",_canPost)
+  document.querySelector('#post-new-post-title').addEventListener("keyup",_canPost)
+
+  //-- IF SUBMIT HAPPENS, SEE IF GOOD TO POST
+  document.querySelector('.post-new-post').onsubmit = event => {
     event.preventDefault();
     postNewPost(event);
-};
+  };
+});
